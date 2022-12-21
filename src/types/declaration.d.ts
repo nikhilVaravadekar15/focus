62e562b47a8b2179e0bc4b3c7fbf76dd21644fe2
Nikhil Varavadekar <nikhil_varavadekar@BHL000190.acds.net.in>
2022-12-21 17:27:41 +0530

    renamed "images.d.ts" to "declaration.d.ts"

--- src/types/declaration.d.ts
+++ src/types/declaration.d.ts
@@ -1,5 +1,11 @@
+// images
 declare module "*.png";
 declare module "*.jpeg";
 declare module "*.gif";
 declare module "*.svg";
 declare module "*.jpg";
+
+// css 
+declare module '*.scss';
+declare module '*.css';
+declare module "*.module.css";
