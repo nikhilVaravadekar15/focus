11467465da5a7fb24662fa2611c16aa9751f78f7
Nikhil Varavadekar <nikhil_varavadekar@BHL000190.acds.net.in>
2023-02-21 16:10:35 +0000

    "TopSites" added

--- src/pages/insights/component/topSites/TopSites.tsx
+++ src/pages/insights/component/topSites/TopSites.tsx
@@ -1,18 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import "./TopSites.css"
 
+type MostVisitedURL = {
+    title: string
+    url: string
+}
+
 function TopSites() {
+
+    const [topSitesArray, setTopSitesArray] = useState<MostVisitedURL[]>([])
+
+    useEffect(() => {
+        chrome.topSites.get((data: MostVisitedURL[]) => {
+            setTopSitesArray(data)
+        })
+    }, [])
 
     return (
         <div className="TopSites">
             <div className="TopSites__header">
                 <h2>Top Sites</h2>
             </div>
-            <div className="TopSites__list-content">
-                <div className="item" key="">
-                    <span className="number">1</span>
-                    <span className="title">www.google.com</span>
-                </div>
+            <div className="TopSites__body">
+                {
+                    topSitesArray === null || topSitesArray.length != 0 ? (
+                        <div className="TopSites__list-content">
+                            {
+                                topSitesArray.map((site: MostVisitedURL, index: number) => {
+                                    return (
+                                        <div className="item" key={index} title={site.url}>
+                                            <span className="number">{index + 1}</span>
+                                            <a className="title" href={site.url} target="_blank">{site["title"]}</a>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ) : (
+                        <div className="substitute">Please wait for at least 24 hrs</div>
+                    )
+                }
             </div>
         </div>
     )
